3426034b1e6c298405d779caae68d24744a907e0
Tanner Byers <tanner.byers@availity.com>
2022-02-03 00:18:35 -0500

    base

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const recentTweetsMock = JSON.parse(
 );
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 
 // This displays message that the server running and listening to specified port
